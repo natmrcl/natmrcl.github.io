637dc376f1a6fc4ac30a5af0a21ccb9b183bef86
natmrcl <91374949+natmrcl@users.noreply.github.com>
2022-12-22 09:55:20 +0700

    Add files via upload

--- Tugas_CRUD_2/index.php
+++ Tugas_CRUD_2/index.php
@@ -1,1 +1,35 @@
-.
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="main-content">
+    <header>
+        <h3>Pendaftaran Siswa Baru</h3>
+        <h1>SMK Coding</h1>
+    </header>
+    <nav>
+    <h4>Menu</h4>
+        <ul>
+            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="list-siswa.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+    </div>
+
+    <?php if(isset($_GET['status'])): ?>
+    <p>
+        <?php
+            if($_GET['status'] == 'sukses'){
+                echo "Pendaftaran siswa baru berhasil!";
+            } else {
+                echo "Pendaftaran gagal!";
+            }
+        ?>
+    </p>
+<?php endif; ?>
+    </body>
+</html>
